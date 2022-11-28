336b09d2f9f6b8426ae80b10ab400fe8d5623dad
Mateo Gonzalez Marulanda <teogonmar97@gmail.com>
2022-11-28 09:04:59 -0500

    Collision logic and alert

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //Jump class will be created and will be added and removed every time I need it
 const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
 
 function jump () {
     if (dino.classList != "jump") {
@@ -10,6 +11,22 @@ function jump () {
         }, 300);
     }
 }
+
+let isAlive = setInterval(function (){
+    //get current dino Y position
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+
+    //get current cactus X position
+    let cactusLeft = parseInt(
+        window.getComputedStyle(cactus).getPropertyValue("left")
+    );
+
+    //Detect collision
+    if (cactusLeft <50 && cactusLeft > 0 && dinoTop >= 140) {
+    //Collision
+        alert("GAME OVER");
+    }
+}, 10);
 
 //Listener for key press action
 document.addEventListener("keydown", function (event) {
